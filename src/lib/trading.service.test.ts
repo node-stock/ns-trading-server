a28aa8ea8c7c9dd8d8d7102eb35e7b9cfd37fcf1
zlq4863947 <zlq4863947@gmail.com>
2018-01-04 06:25:51 +0900

    代码更新

--- src/lib/trading.service.test.ts
+++ src/lib/trading.service.test.ts
@@ -2,16 +2,17 @@ import { TradingService } from './trading.service';
 import * as assert from 'power-assert';
 import * as types from 'ns-types';
 import { Store as db, Account } from 'ns-store';
+import { BitbankApiOrder } from 'bitbank-handler';
 
 const config = require('config');
 const tradingServ = new TradingService();
 const testBuy = async () => {
   const order: types.LimitOrder = {
-    price: '1940063',
+    price: '1630063',
     symbol: 'btc_jpy',
     symbolType: types.SymbolType.cryptocoin,
     orderType: types.OrderType.Limit,
-    tradeType: types.TradeType.Margin,
+    tradeType: types.TradeType.Spot,
     side: types.OrderSide.Buy,
     amount: '0.0001',
     eventType: types.EventType.Order,
@@ -20,14 +21,13 @@ const testBuy = async () => {
   await tradingServ.order('test', order);
 }
 
-
 const testSell = async () => {
   const order: types.LimitOrder = {
-    price: '2197063',
+    price: '1680063',
     symbol: 'btc_jpy',
     symbolType: types.SymbolType.cryptocoin,
     orderType: types.OrderType.Limit,
-    tradeType: types.TradeType.Margin,
+    tradeType: types.TradeType.Spot,
     side: types.OrderSide.BuyClose,
     amount: '0.00001',
     eventType: types.EventType.Order,
@@ -36,13 +36,39 @@ const testSell = async () => {
   await tradingServ.order('test', order);
 }
 
+const testSaveRecord = async () => {
+  const srcOrder: types.Order = {
+    symbol: types.Pair.BTC_JPY,
+    price: '1675000',
+    amount: '0.00421125',
+    symbolType: types.SymbolType.cryptocoin,
+    eventType: types.EventType.Order,
+    tradeType: types.TradeType.Spot,
+    orderType: types.OrderType.Limit,
+    side: types.OrderSide.BuyClose,
+    backtest: '1'
+  };
+  const order: BitbankApiOrder = {
+    "order_id": 2534449,
+    "pair": "bcc_jpy",
+    "side": "buy",
+    "type": "limit",
+    "start_amount": "0.01000000",
+    "remaining_amount": "0.01000000",
+    "executed_amount": "0.00000000",
+    "price": "291856.0000",
+    "average_price": "0.0000",
+    "ordered_at": 1514893684014,
+    "status": "UNFILLED"
+  };
+  await tradingServ.saveRecord(order, srcOrder);
+}
+
 describe('TradingService测试', () => {
-  before(async () => {
-    await db.init(require('config').store);
-  });
-  it('测试买入', testBuy);
+  // it('测试买入', testBuy);
   // it('测试卖出', testSell);
+  it('测试记录下单', testSaveRecord);
   after(async () => {
-    await db.close();
+    await tradingServ.destroy();
   });
 });
--- src/lib/trading.service.ts
+++ src/lib/trading.service.ts
@@ -5,14 +5,12 @@ import * as types from 'ns-types';
 import { OrderManager } from 'ns-manager';
 import { Log, Util } from 'ns-common';
 import { WebTrader } from 'ns-trader';
-import { Bitbank, BitbankApiAsset } from 'bitbank-handler';
+import { Bitbank, BitbankApiAsset, BitbankApiOrder } from 'bitbank-handler';
 import { BigNumber } from 'BigNumber.js';
+import { Store as db } from 'ns-store';
 
 const config = require('config');
-const bitbank = new Bitbank({
-  apiKey: config.trader.apiKey,
-  apiSecret: config.trader.secret
-});
+db.init(config.store);
 Log.init(Log.category.system, Log.level.ALL, 'ns-trading-server');
 
 @Component()
@@ -25,77 +23,91 @@ export class TradingService {
     this.trader.init();
   }
 
-  async order(accountId: string, orderInfo: types.LimitOrder) {
+  async order(accountId: string, orderInfo: types.Order) {
+    const bitbank = new Bitbank({
+      apiKey: config.trader.apiKey,
+      apiSecret: config.trader.secret
+    });
     try {
       Log.system.info('下单[启动] ', orderInfo);
-      let res;
       let amount = Number(orderInfo.amount);
-      if (orderInfo.backtest !== '1') {
+      // if (orderInfo.backtest !== '1') {
 
-        if (orderInfo.side === types.OrderSide.Buy) {
-          // 买入多单
-          if (orderInfo.symbolType === types.SymbolType.cryptocoin) {
-            res = await bitbank.createOrder({
-              pair: orderInfo.symbol,
-              amount,
-              price: Number(orderInfo.price),
-              side: types.OrderSide.Buy,
-              type: orderInfo.orderType
-            }).toPromise();
-            Log.system.info('买入多单结果：', JSON.stringify(res, null, 2));
-          } else {
-            await this.trader.buy(orderInfo);
-          }
-        } else if (orderInfo.side === types.OrderSide.BuyClose) {
-          // 卖出多单
-          if (orderInfo.symbolType === types.SymbolType.cryptocoin) {
-            // 
-            const assetsRes = await bitbank.getAssets().toPromise();
-            const assetType = Util.getTradeAssetType(orderInfo.symbol);
-            const asset = <BitbankApiAsset>assetsRes.assets.find(o => o.asset === assetType);
-            const freeAmount = new BigNumber(asset.free_amount);
-            const orderAmount = new BigNumber(orderInfo.amount);
-            amount = freeAmount.lessThan(orderAmount) ? freeAmount.toNumber() : orderAmount.toNumber();
-            res = await bitbank.createOrder({
-              pair: orderInfo.symbol,
-              amount,
-              price: Number(orderInfo.price),
-              side: types.OrderSide.Sell,
-              type: orderInfo.orderType
-            }).toPromise();
-            Log.system.info('卖出多单结果：', JSON.stringify(res, null, 2));
-          } else {
-            await this.trader.sell(orderInfo);
-          }
-        } else if (orderInfo.side === types.OrderSide.Sell) {
-          Log.system.error('买入空单，尚未实现！');
-          // 买入空单
-          // TODO
-        } else if (orderInfo.side === types.OrderSide.SellClose) {
-          Log.system.error('卖出空单，尚未实现！');
-          // 卖出空单
-          // TODO
+      if (orderInfo.side === types.OrderSide.Buy) {
+        // 买入多单
+        if (orderInfo.symbolType === types.SymbolType.cryptocoin) {
+          const res = await bitbank.createOrder({
+            pair: orderInfo.symbol,
+            amount,
+            price: Number(orderInfo.price),
+            side: types.OrderSide.Buy,
+            type: orderInfo.orderType
+          }).toPromise();
+          Log.system.info('买入多单结果：', JSON.stringify(res, null, 2));
+          // 记录订单
+          await this.saveRecord(res, orderInfo);
+        } else {
+          await this.trader.buy(orderInfo);
         }
+      } else if (orderInfo.side === types.OrderSide.BuyClose) {
+        // 卖出多单
+        if (orderInfo.symbolType === types.SymbolType.cryptocoin) {
+          // 
+          const assetsRes = await bitbank.getAssets().toPromise();
+          const assetType = Util.getTradeAssetType(orderInfo.symbol);
+          const asset = <BitbankApiAsset>assetsRes.assets.find(o => o.asset === assetType);
+          const freeAmount = new BigNumber(asset.free_amount);
+          const orderAmount = new BigNumber(orderInfo.amount);
+          amount = freeAmount.lessThan(orderAmount) ? freeAmount.toNumber() : orderAmount.toNumber();
+          const res = await bitbank.createOrder({
+            pair: orderInfo.symbol,
+            amount,
+            price: Number(orderInfo.price),
+            side: types.OrderSide.Sell,
+            type: orderInfo.orderType
+          }).toPromise();
+          Log.system.info('卖出多单结果：', JSON.stringify(res, null, 2));
+          // 记录订单
+          await this.saveRecord(res, orderInfo);
+        } else {
+          await this.trader.sell(orderInfo);
+        }
+      } else if (orderInfo.side === types.OrderSide.Sell) {
+        Log.system.error('买入空单，尚未实现！');
+        // 买入空单
+        // TODO
+      } else if (orderInfo.side === types.OrderSide.SellClose) {
+        Log.system.error('卖出空单，尚未实现！');
+        // 卖出空单
+        // TODO
       }
-      if (res['order_id']) {
-        const order: types.Model.Order = Object.assign({}, orderInfo, {
-          id: res['order_id'],
-          account_id: 'coin',
-          quantity: orderInfo.amount,
-          status: types.OrderStatus.Unfilled,
-          type: orderInfo.symbolType
-        });
-        Log.system.info('记录订单信息：', JSON.stringify(order, null, 2));
-        // 记录订单信息
-        await OrderManager.set(order);
-      }
+      // }
       Log.system.info('下单[终了]');
     } catch (e) {
       Log.system.error('下单异常[终了] ', e.stack);
     }
   }
 
-  destroy() {
-    this.trader.end();
+  async saveRecord(order: BitbankApiOrder, srcOrder: types.Order) {
+    const dbOrder: types.Model.Order = {
+      id: String(order.order_id),
+      account_id: 'coin',
+      quantity: order.start_amount,
+      price: order.price,
+      status: types.OrderStatus.Unfilled,
+      side: srcOrder.side,
+      symbol: order.pair,
+      type: srcOrder.symbolType,
+      backtest: srcOrder.backtest,
+      mocktime: srcOrder.mocktime
+    };
+    Log.system.info('记录订单信息：', JSON.stringify(order, null, 2));
+    // 记录订单信息
+    await OrderManager.set(dbOrder);
+  }
+
+  async destroy() {
+    await db.close();
+    await this.trader.end();
   }
 }
